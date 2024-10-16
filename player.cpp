7ee32571d6f08f334868954620b4d86a00e44f0b
alinmaslov55 <maslovalin55@gmail.com>
2024-10-16 15:22:00 +0300

    corectat player.cpp

--- player.cpp
+++ player.cpp
@@ -1,10 +1,13 @@
 #include "player.h"
 #include <vector>
+#include <string>
 
 const float initial_money = 1000;
 
 class Player{
     private:
+        int playerID;
+        std::string playerName;
         float money;
         int rounds_played;
         float total_money_lost;
@@ -12,7 +15,7 @@ class Player{
         std::vector<float> history_of_rounds;
 
     public:
-        Player(){
+        Player(int ID, std::string name): playerID(ID), playerName(name){
             money = initial_money;
             rounds_played = 0;
             total_money_won = total_money_lost = 0;
