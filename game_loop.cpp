3a88dd101ee0081bda9bd2e0f981429154b6778e
alinmaslov55 <maslovalin55@gmail.com>
2024-10-16 15:10:15 +0300

    modificari

---
+++ game_loop.cpp
@@ -0,0 +1,9 @@
+#include <game_loop.h>
+
+void set_player(){
+
+}
+void game_loop(){
+
+    
+}
---
+++ game_loop.h
@@ -0,0 +1,9 @@
+#include <iostream>
+
+#ifndef GAME_LOOP
+#define GAME_LOOP
+
+void set_player();
+void game_loop();
+
+#endif
