f5d23c813302ed05b905dbe18c3800a254bfc6a9
alinmaslov55 <maslovalin55@gmail.com>
2024-10-17 22:09:37 +0300

    modificari

--- print_to_screen.cpp
+++ print_to_screen.cpp
@@ -1,21 +1,23 @@
 #include "print_to_screen.h"
-
+#include <vector>
 const std::string line = "----------------------------------------------------------------";
 int show_menu(){
     int option = -1;
     while(!(option >= 0 && option <= 3)){
+        std::cout << line << std::endl;
         std::cout << "1. Play a Round" << std::endl;
         std::cout << "2. Show statistics" << std::endl;
         std::cout << "0. Exit" << std::endl;
         std::cout << "Choose option: " << std::endl;
         std::cin >> option;
+        std::cout << line << std::endl;
     }
     return option;
 }
 void print_every_player_status(std::vector<Player> &vectorPlayers){
-    for(int i = 0; i < vectorPlayers.size(); i++){
+    for(auto player : vectorPlayers){
         std::cout << line << std::endl;
-        std::cout << vectorPlayers[i] << std::endl;
+        std::cout << player << std::endl;
         std::cout << line << std::endl;
     }
 }
